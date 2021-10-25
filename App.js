9ae56b6cb22db89b2021eeeedc9e3ea7b29e056f
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2021-10-25 11:07:42 -0400

    started app.js

--- App.js
+++ App.js
@@ -1,3 +1,42 @@
-const fs = require('fs');
-const mysql = require('mysql');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3001,
+  user: "root",
+  password: "root",
+  database: "employeetracker_db",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  runApp();
+});
+
+function runApp() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What would you like to do?",
+        choices: [
+          "View all employees",
+          "View all employees by department",
+          "View all employees by manager",
+          "Add Employee",
+          "Remove Employee",
+          "Update Employee Role",
+          "Update Employee Manager",
+          "Exit",
+        ],
+      },
+    ])
+    .then(function (res) {
+      if (res.choice === "View all employees") {
+          view.viewAllEmployees();
+      }
+    });
+}
