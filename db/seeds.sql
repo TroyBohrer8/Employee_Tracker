89874230712beb9b1669a8ba55179ff55a7dda7a
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2021-11-03 14:11:36 -0400

    changed seeds data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,22 @@
-INSERT INTO department (dept_name) VALUES ('Back of House'), ('Front of House'), ('Management');
-INSERT INTO company_role (title, salary, dept_id) VALUES
-('General Manager', 80000.00, 3), 
-('Head Chef', 60000.00, 1),
-('Sous Chef', 30000.00, 1),
-('Cook', 30000.00, 1 ),
-('Dishwasher', 20000.00, 1),                
-('Front of House Manager', 40000.00, 2),
-('Host', 30000.00, 2),
-('Server', 20000.00, 1);
+INSERT INTO department (dept_id, dept_name)
+VALUES (1, "Plumber"),
+    (2, "Carpenter"),
+    (3, "Electrician"),
+    (4, "Laborer"),
+    (5, "Project Managment");
+INSERT INTO role (role_id, title, salary, dept_id)
+VALUES (01, "Plumber", 90000, 1),
+    (02, "Carpenter", 60000, 2),
+    (03, "Electrician", 100000, 3),
+    (04, "Laborer", 40000, 4),
+    (05, "Project Manager", 120000, 5);
+INSERT INTO employee (emp_id, first_name, last_name, emp_role_id, manager_id)
+VALUES (001, "John", "Smith", 01, NULL),
+    (002, "Tony", "Cook", 02, NULL),
+    (003, "DeSean", "Pratt", 03, NULL),
+    (004, "Carlos", "Sanchez", 04, NULL),
+    (005, "Brandon", "Mayhew", 05, 001);
 
-INSERT INTO employees (first_name, last_name, emp_role_id, manager_id) VALUES
-('Greg', 'Sampson', 1, null),
-('Chris', 'Higgs', 6, 1),
-('Troy', 'Martin', 2, null);
+SELECT * FROM department;
+SELECT * FROM employee;
+SELECT * FROM role;
